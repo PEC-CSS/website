380a550cc1522cab47a5fcbf290d404bb1d757f8
harsh johar <hpsj3112@gmail.com>
2022-12-21 02:53:15 +0530

    desktop navbar and other files

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,20 +1,76 @@
-import { useState } from 'react';
-import styles from '../../../styles/components/Header.module.scss'
+import Link from "next/link";
+import { useState } from "react";
+import styles from "../../../styles/components/Header.module.scss";
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo_wrapper}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/vercel.svg" alt="helo" />
-      </div>
-      <div className={styles.list_items_wrapper}>
+    const [menuOpen, setMenuOpen] = useState(false);
 
-      </div>
-    </nav>
-  )
+    function toggleMenu() {
+        setMenuOpen(!menuOpen);
+    }
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo_wrapper}>
+                <Link href={"/"}>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img src="/vercel.svg" alt="ACM at PEC" />
+                </Link>
+            </div>
+            <div className={styles.list_items_wrapper}>
+                {/* hamburger icon */}
+                <svg
+                    className={`${styles.ham} ${menuOpen ? styles.active : ""}`}
+                    viewBox="0 0 100 100"
+                    width="80"
+                    onClick={toggleMenu}
+                >
+                    <path
+                        className={`${styles.line} ${styles.top}`}
+                        d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
+                    />
+                    <path
+                        className={`${styles.line} ${styles.middle}`}
+                        d="m 70,50 h -40"
+                    />
+                    <path
+                        className={`${styles.line} ${styles.bottom}`}
+                        d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
+                    />
+                </svg>
+                {/* hamburger end */}
+                <div className={`${menuOpen ? styles.active : ""} menu_modal`}>
+                    <ul
+                        className={`${styles.nav_items} ${
+                            menuOpen ? styles.active : ""
+                        }`}
+                        role="presentation"
+                    >
+                        <li onClick={toggleMenu}>
+                          <Link href={'/about'} className={styles.nav_link}>
+                            about
+                          </Link>
+                        </li>
+                        <li onClick={toggleMenu}>
+                          <Link href={'/committees'} className={styles.nav_link}>
+                            committies
+                          </Link>
+                        </li>
+                        <li onClick={toggleMenu}>
+                          <Link href={'/events'} className={styles.nav_link}>
+                            events
+                          </Link>
+                        </li>
+                        <li>
+                          <button>
+                            Member Login
+                          </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import PageLayout from '../components/layout/PageLayout'
 export default function Home() {
   return (
     <PageLayout title='Home'>
-      Home
+      
     </PageLayout>
   )
 }
