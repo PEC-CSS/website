e87b2faed2da25381f6e5d68de8a7e0d8f1cb80a
harsh johar <hpsj3112@gmail.com>
2022-12-22 02:07:41 +0530

    ham pls fix

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -19,27 +19,35 @@ function Header() {
             </div>
             <div className={styles.list_items_wrapper}>
                 {/* hamburger icon */}
-                <svg
-                    className={`${styles.ham} ${menuOpen ? styles.active : ""}`}
-                    viewBox="0 0 100 100"
-                    width="80"
-                    onClick={toggleMenu}
-                >
-                    <path
-                        className={`${styles.line} ${styles.top}`}
-                        d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
-                    />
-                    <path
-                        className={`${styles.line} ${styles.middle}`}
-                        d="m 70,50 h -40"
-                    />
-                    <path
-                        className={`${styles.line} ${styles.bottom}`}
-                        d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
-                    />
-                </svg>
+                <div className={styles.hamburger_wrapper}>
+                    <svg
+                        className={`${styles.ham} ${
+                            menuOpen ? styles.active : ""
+                        }`}
+                        viewBox="0 0 100 100"
+                        width="80"
+                        onClick={toggleMenu}
+                    >
+                        <path
+                            className={`${styles.line} ${styles.top}`}
+                            d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
+                        />
+                        <path
+                            className={`${styles.line} ${styles.middle}`}
+                            d="m 70,50 h -40"
+                        />
+                        <path
+                            className={`${styles.line} ${styles.bottom}`}
+                            d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
+                        />
+                    </svg>
+                </div>
                 {/* hamburger end */}
-                <div className={`${menuOpen ? styles.active : ""} menu_modal`}>
+                <div
+                    className={`${menuOpen ? styles.active : ""} ${
+                        styles.menu_modal
+                    }`}
+                >
                     <ul
                         className={`${styles.nav_items} ${
                             menuOpen ? styles.active : ""
@@ -47,24 +55,25 @@ function Header() {
                         role="presentation"
                     >
                         <li onClick={toggleMenu}>
-                          <Link href={'/about'} className={styles.nav_link}>
-                            about
-                          </Link>
+                            <Link href={"/about"} className={styles.nav_link}>
+                                about
+                            </Link>
                         </li>
                         <li onClick={toggleMenu}>
-                          <Link href={'/committees'} className={styles.nav_link}>
-                            committies
-                          </Link>
+                            <Link
+                                href={"/committees"}
+                                className={styles.nav_link}
+                            >
+                                committies
+                            </Link>
                         </li>
                         <li onClick={toggleMenu}>
-                          <Link href={'/events'} className={styles.nav_link}>
-                            events
-                          </Link>
+                            <Link href={"/events"} className={styles.nav_link}>
+                                events
+                            </Link>
                         </li>
                         <li>
-                          <button>
-                            Member Login
-                          </button>
+                            <button>Member Login</button>
                         </li>
                     </ul>
                 </div>
