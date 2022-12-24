b147d9b43977abe7f824b2635b49d4949d0b47e9
harsh johar <hpsj3112@gmail.com>
2022-12-24 19:57:29 +0530

    about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import PageLayout from "../components/layout/PageLayout";
+import styles from '../styles/pages/about.module.scss';
 
 function about() {
-    return <PageLayout title="About  | ACM at PEC"></PageLayout>;
+    return <PageLayout title="About  | ACM at PEC">
+
+    </PageLayout>;
 }
 
 export default about;
