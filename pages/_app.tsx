831cf3ca852a98b4057c9ff6ab9ea3c1ea033e5e
byjus-dhruv-bansal <dhruv.bansal@byjus.com>
2023-07-06 04:14:33 +0530

    new repo creation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
+
+// bansal entry
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
