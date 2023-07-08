4983852503756eb8ecd06c87df98f998b1294cc2
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2023-07-08 16:30:19 +0530

    login page

---
+++ pages/login.tsx
@@ -0,0 +1,103 @@
+import React, { ChangeEvent, useState } from 'react';
+import PageLayout from "../components/layout/PageLayout";
+import styles from "../styles/pages/register.module.scss";
+import TextField from '@mui/material/TextField';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { BiErrorCircle } from "react-icons/bi"
+import ErrorTextBox from '../components/register/ErrorTextBox';
+
+function Login() {
+
+    const [formValues, setFormValues] = useState({
+        username: '',
+        password: '',
+    })
+
+    const [error, setError] = useState(false);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>
+        | SelectChangeEvent
+        | React.ChangeEvent<HTMLTextAreaElement
+            | HTMLInputElement>) => {
+        event.preventDefault();
+        const name = event.target.name;
+        const value = event.target.value;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // check if login credentials are valid
+        if (true) {
+            setError(true);
+
+            setTimeout(() => {
+                setError(false);
+            }, 3000);
+        } else {
+            // make API calls
+        }
+    }
+
+    return (
+        <PageLayout title="Login | ACM at PEC">
+
+            {/* Header */}
+            <form className={styles.parent} onSubmit={handleSubmit}>
+
+                <div className={styles.intro}>
+                    <h1>Login</h1>
+                </div>
+
+                {/* User Account Details */}
+                <div className={styles.sectionWrapper}>
+
+                    <div className={styles.flowSection}>
+                        <div className={styles.accountDetails}>
+                            <TextField
+                                name="username"
+                                label="Username"
+                                variant="filled"
+                                onChange={handleChange}
+                                value={formValues.username}
+                                fullWidth
+                                required />
+                        </div>
+                        <div className={styles.accountDetails}>
+                            <TextField
+                                name="password"
+                                label="Password"
+                                variant="filled"
+                                type="password"
+                                onChange={handleChange}
+                                value={formValues.password}
+                                fullWidth
+                                required />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Submit Button */}
+                <div className={`${styles.buttonGroup} ${styles.flowSection}`}>
+                    {error
+                        ? <ErrorTextBox text={errorText.incorrectEntry} icon={<BiErrorCircle />} />
+                        : <></>
+                    }
+                    <div className={styles.registerButton}>
+                        <button type="submit">Login</button>
+                    </div>
+                </div>
+            </form>
+        </PageLayout>
+    )
+}
+
+export default Login;
+
+const errorText = {
+    incorrectEntry: "Login credentials are incorrect."
+}
