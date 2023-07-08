6272df86190e2bab3fc3218736a1bffbc3e20b01
Ishwarendra Jha <ishwarendra27@gmail.com>
2023-07-09 02:24:31 +0530

    #8 About us page and card fix, Remove console logs

--- components/about/AboutCard.tsx
+++ components/about/AboutCard.tsx
@@ -2,24 +2,32 @@ import styles from "../../styles/pages/about.module.scss";
 
 type Props = {
     title: String;
-    description: String;
+    descriptions: String[];
     imgUrl: String;
     imgAlt?: String;
     flip?: boolean;
 };
 
-function AboutCard({ title, description, imgUrl, imgAlt, flip }: Props) {
+function AboutCard({ title, descriptions, imgUrl, imgAlt, flip = false }: Props) {
     return (
-        <div className={`${styles.card} ${flip==true ? styles.flip : ""}`}>
-            <div className={styles.left}>
-                <h1 className={styles.title}>{title}</h1>
-                <p className={styles.description}>{description}</p>
+        <div className={`${styles.card} ${flip ? styles.flip : ''}`}>
+            <div className={styles.content}>
+                <h1>{title}</h1>
+                {
+                    descriptions.map((description, id) =>
+                        <div key={id} className={`${styles.text} ${flip ? styles.flip : ''}`}>
+                            {description}
+                        </div>)
+                }
             </div>
-            <div className={styles.right}>
+
+            <div className={styles.image}>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                     src={imgUrl as string}
                     alt={imgAlt ? (imgAlt as string) : ""}
+                    width={270}
+                    height={210}
                 />
             </div>
         </div>
---
+++ constants/aboutUsText.ts
@@ -0,0 +1,42 @@
+export const aboutUsText = [
+    {
+        title: 'Who we are?',
+        descriptions: [
+            'PEC ACM is the official student chapter of the Association for Computing Machinery (ACM) at our college. As an ACM chapter, our responsibilities include organizing technical workshops, hosting coding competitions, conducting guest lectures, and fostering a community of passionate computer science enthusiasts.',
+            'Our team works tirelessly to promote and enhance coding skills among students through various initiatives and events. Additionally, we collaborate with different companies on specific projects, bridging the gap between academia and industry.'
+        ],
+        imgUrl: '',
+        imgAlt: 'PEC ACM Logo',
+        key: 1
+    },
+    {
+        title: 'How we operate?',
+        descriptions: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla nisi ut neque consectetur, at tristique tellus consectetur. Integer dapibus nunc at lacus consequat commodo.',
+            'Pellentesque ac metus in est tristique pulvinar. Vestibulum euismod dolor nec tincidunt ultrices. Maecenas non velit et massa dictum aliquam sed id lectus.'
+        ],
+        imgUrl: '',
+        imgAlt: '',
+        key: 2
+    },
+    {
+        title: 'Our Mission',
+        descriptions: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla nisi ut neque consectetur, at tristique tellus consectetur. Integer dapibus nunc at lacus consequat commodo.',
+            'Pellentesque ac metus in est tristique pulvinar. Vestibulum euismod dolor nec tincidunt ultrices. Maecenas non velit et massa dictum aliquam sed id lectus.'
+        ],
+        imgUrl: '',
+        imgAlt: 'PEC Logo',
+        key: 3
+    },
+    {
+        title: 'Our Vision',
+        descriptions: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla nisi ut neque consectetur, at tristique tellus consectetur. Integer dapibus nunc at lacus consequat commodo.',
+            'Pellentesque ac metus in est tristique pulvinar. Vestibulum euismod dolor nec tincidunt ultrices. Maecenas non velit et massa dictum aliquam sed id lectus.'
+        ],
+        imgUrl: '',
+        imgAlt: '',
+        key: 4
+    }
+]
