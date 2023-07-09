6137c9c5cea49dbe913b90ef7049db8c7b732db8
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-07-09 17:12:33 +0530

    team page : core and heads

---
+++ constants/team.ts
@@ -0,0 +1,72 @@
+const HEADS = [
+    {
+        name: "Harshpreet Singh Johar",
+        image: "/assets/images/team/heads/harshpreet.jpeg",
+        post: "Chair",
+    },
+    {
+        name: "Manjot Singh Oberoi",
+        image: "/assets/images/team/heads/manjot.jpeg",
+        post: "Vice Chair",
+    },
+    {
+        name: "Ishwarendra Jha",
+        image: "/assets/images/team/heads/ishwarendra.jpeg",
+        post: "Secretary/Treasurer",
+    },
+    {
+        name: "Kriti Mahajan",
+        image: "/assets/images/team/heads/kriti.jpeg",
+        post: "Webmaster",
+    },
+];
+
+const CORE = [
+    {
+        name: "Abhinav Rawal",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Uttam Mittal",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Vinayak Gupta",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Dhruv Bansal",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Kavya Rishi",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Anuj Awasthi",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Prakhar Tomar",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Harasees Singh",
+        image: "",
+        post: "",
+    },
+    {
+        name: "Ashutosh Aswal",
+        image: "",
+        post: "",
+    },
+];
+
+export { HEADS, CORE };
