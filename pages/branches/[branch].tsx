4ee22ac2b752299eb7cb1ed2614968986b9976f1
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-07-12 00:26:45 +0530

    dynamic css

--- pages/branches/[branch].tsx
+++ pages/branches/[branch].tsx
@@ -2,30 +2,33 @@ import React from "react";
 import PageLayout from "../../components/layout/PageLayout";
 import { BRANCHES } from "../../constants/branch";
 import { useRouter } from "next/router";
+import styles from "../../styles/pages/branch.module.scss";
 
-export default function Branch({branch}: {branch: string}) {
+export default function Branch({ branch }: { branch: string }) {
     const router = useRouter();
 
-    return <PageLayout title={`${branch} | ACM at PEC`}>
-        {branch}
-    </PageLayout>
+    return (
+        <PageLayout title={`${branch} | ACM at PEC`}>
+            <div className={styles.branch} data-branch={branch}>{branch}</div>
+        </PageLayout>
+    );
 }
 
-export async function getServerSideProps({query}: any) {
+export async function getServerSideProps({ query }: any) {
     const branch = query.branch;
 
-    if(!Object.keys(BRANCHES).includes(branch)) {
+    if (!Object.keys(BRANCHES).includes(branch)) {
         return {
             redirect: {
                 destination: "/404",
                 permanent: false,
             },
-        }
+        };
     }
 
     return {
         props: {
-            branch: branch
-        }
-    }
+            branch: branch,
+        },
+    };
 }
