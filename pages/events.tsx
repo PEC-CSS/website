1b0f28ba4c96af3438816da19ec127753f437075
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-07-13 01:37:43 +0530

    events page -- prelim stage

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import PageLayout from "../components/layout/PageLayout";
+import { Calendar, Event, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
-function events() {
-    return <PageLayout title="Events | ACM at PEC"></PageLayout>;
+const localizer = momentLocalizer(moment);
+
+import styles from "../styles/pages/events.module.scss";
+import "react-big-calendar/lib/css/react-big-calendar.css"; // calendar css
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+
+type NavigateAction = "PREV" | "NEXT" | "TODAY" | "DATE";
+
+function Events() {
+    const [listedEvents, setListedEvents] = useState<Event[] | undefined>([
+        {
+            start: moment().toDate(),
+            end: moment().add(17, "hours").toDate(),
+            title: "sex sux",
+            resource: "huehheuuheu",
+        },
+    ]);
+
+    const [activeEvent, setActiveEvent] = useState<Event | null>(null);
+
+    return (
+        <PageLayout title="Events | ACM at PEC" heading="Events">
+            <div className={styles.events}>
+                <div className={styles.toolbar}>
+                    <div className={styles.left}>
+                        <BiChevronLeft className={styles.icon} />
+                        <BiChevronRight className={styles.icon} />
+                        <p>Month XXXX</p>
+                    </div> 
+                    <div className={styles.right}>{/* MUI button grp */}</div>
+                </div>
+                <div className={styles.calendarWrapper}>
+                    <Calendar
+                        localizer={localizer}
+                        events={listedEvents}
+                        className={styles.calendar}
+                        popup={true}
+                        onSelectEvent={setActiveEvent}
+                        startAccessor={(event) =>
+                            new Date(event.start ?? Date.now())
+                        }
+                        endAccessor={(event) =>
+                            new Date(event.end ?? Date.now())
+                        }
+                        views={{
+                            month: true,
+                            week: true,
+                            day: true,
+                        }}
+                        dayLayoutAlgorithm={"no-overlap"}
+                        toolbar={false}
+                    />
+                </div>
+            </div>
+        </PageLayout>
+    );
 }
 
-export default events;
+export default Events;
