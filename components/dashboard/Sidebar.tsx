b5fdb22ee944085dafde85ac1d3e6d18697cac20
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2023-07-14 22:51:29 +0530

    Completed responsiveness of sidebar

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -3,8 +3,10 @@ import {SocialLinks} from "../common/SocialLinks/SocialLinks";
 import {FaHome} from "react-icons/fa";
 import {MdAccountCircle, MdLogout} from "react-icons/md";
 import {BiCalendarEvent} from "react-icons/bi";
-import {AiFillTrophy} from "react-icons/ai";
+import {AiFillTrophy, AiOutlineMenuUnfold} from "react-icons/ai";
 import {SidebarItem} from "./SidebarItem";
+import {useState} from "react";
+import {CgClose} from "react-icons/cg";
 
 const sidebarItems = [
     {
@@ -42,25 +44,40 @@ export const Sidebar = () => {
         dp: "https://avatars.githubusercontent.com/u/78747188?v=4"
     }
 
+    const [mobileSidebarOpen, setMobileSidebarOpen] = useState("false")
+
     return (
-        <div className={styles.sidebar}>
-            <div className={styles.header}>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={user.dp} alt={""} />
-                <h2>{user.name}</h2>
-                <h4>{user.designation}</h4>
+        <>
+            <AiOutlineMenuUnfold
+                onClick={() => setMobileSidebarOpen("true")}
+                className={styles.hamburger}
+            />
+            <div className={styles.sidebar} data-open={mobileSidebarOpen}>
+                <div className={styles.header}>
+                    <div className={styles.pfp}>
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img src={user.dp} alt={""} />
+                        <CgClose
+                            onClick={() => setMobileSidebarOpen("false")}
+                            className={styles.close}
+                        />
+                    </div>
+
+                    <h2>{user.name}</h2>
+                    <h4>{user.designation}</h4>
+                </div>
+                <div className={styles.items}>
+                    {
+                        sidebarItems.map((item, index) => {
+                            return <SidebarItem key={index} title={item.title} icon={item.icon} path={item.path} />
+                        })
+                    }
+                </div>
+                <div className={styles.socialLinks}>
+                    <h2>Social Media</h2>
+                    <SocialLinks />
+                </div>
             </div>
-            <div className={styles.items}>
-                {
-                    sidebarItems.map((item, index) => {
-                        return <SidebarItem key={index} title={item.title} icon={item.icon} path={item.path} />
-                    })
-                }
-            </div>
-            <div className={styles.socialLinks}>
-                <h2>Social Media</h2>
-                <SocialLinks />
-            </div>
-        </div>
+        </>
     )
 }
---
+++ pages/dashboard/events.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../../components/layout/DashboardLayout";
+
+function Events() {
+    return (
+        <DashboardLayout title="Events | ACM at PEC" heading="Events">
+            EVENTS
+        </DashboardLayout>
+    )
+}
+
+export default Events;
