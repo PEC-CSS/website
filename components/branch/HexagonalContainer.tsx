b4e989a202b2ae2eba67608e086392d690e4964a
Ishwarendra Jha <ishwarendra27@gmail.com>
2023-07-20 01:13:57 +0530

    Add hexagon barebone

---
+++ components/branch/HexagonalContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hexagon from "react-hexagon";
+// import ''
+
+export default function HexagonalContainer() {
+    return (
+        <div>
+            <Hexagon>
+                <img src="../../public/assets/logos/acmSVG.svg" height={200} width={200} />
+            </Hexagon>
+        </div>
+    )
+}
--- pages/branches/index.tsx
+++ pages/branches/index.tsx
@@ -1,10 +1,11 @@
+import HexagonalContainer from "../../components/branch/HexagonalContainer";
 import PageLayout from "../../components/layout/PageLayout";
 
 export default function Branches() {
     return (
         <PageLayout title={"Branches | ACM at PEC"} heading={"Branches"}>
             <div>
-               
+                <HexagonalContainer />
             </div>
         </PageLayout>
     )
