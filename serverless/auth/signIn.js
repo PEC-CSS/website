bcf8bfdace07fd3c8ee3760090e39463fb09f98d
Gopal Verma <gopalverma@Gopals-MacBook-Air.local>
2023-08-30 23:28:33 +0530

    added firebase

---
+++ serverless/auth/signIn.js
@@ -0,0 +1,14 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../firebase';
+
+export default async function signIn(email, password) {
+    let result = null,
+        error = null;
+    try {
+        result = await signInWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
---
+++ serverless/auth/signUp.js
@@ -0,0 +1,14 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../firebase';
+
+export default async function signUp(email, password) {
+    let result = null,
+        error = null;
+    try {
+        result = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
