8654616a3a49985fcfe8ccdc3fa0e6a2d7082e1e
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-09-02 10:18:46 +0530

    develompers

--- constants/team.ts
+++ constants/team.ts
@@ -89,7 +89,55 @@ const CORE = [
         image: `${folderPath}/Saniya Singla.jpg`,
         post: "Socials Co-Lead",
     },
-    
 ];
 
-export { HEADS, CORE };
+const rootTechPath = "/assets/illustrations/tech";
+const DEVELOPERS = [
+    {
+        name: "Harshpreet Singh Johar",
+        image: "/assets/images/team/heads/harshpreet.jpeg",
+        post: "Full Stack",
+        tech: getTechAssets(["nextjs", "spring", "supabase", "do", "fi"]),
+    },
+    {
+        name: "Manjot Singh Oberoi",
+        image: "/assets/images/team/heads/manjot.jpeg",
+        post: "Full Stack",
+        tech: getTechAssets(["nextjs", "spring", "supabase"]),
+    },
+    {
+        name: "Sanil Gupta",
+        image: `${folderPath}/Sanil_Gupta.jpeg`,
+        post: "Backend",
+        tech: getTechAssets(["spring", "supabase"]),
+    },
+    {
+        name: "Abhinav Rawal",
+        image: `${folderPath}/Abhinav_Rawal.jpg`,
+        post: "Backend",
+        tech: getTechAssets(["spring", "supabase"]),
+    },
+    {
+        name: "Ishwarendra Jha",
+        image: "/assets/images/team/heads/ishwarendra.jpeg",
+        post: "Frontend",
+        tech: getTechAssets(["nextjs", "fi"]),
+    },
+    {
+        name: "Kriti Mahajan",
+        image: "/assets/images/team/heads/kriti.jpeg",
+        post: "Designing, UI/UX",
+        tech: getTechAssets(["nextjs", "fi", "ai"]),
+    },
+];
+
+function getTechAssets(names: string[]): string[] {
+    let assets = [];
+    for (let index = 0; index < names.length; index++) {
+        const element = names[index];
+        assets.push(`${rootTechPath}/${element}.svg`);
+    }
+    return assets;
+}
+
+export { HEADS, CORE, DEVELOPERS };
--- pages/developers.tsx
+++ pages/developers.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import PageLayout from '../components/layout/PageLayout'
+import React from "react";
+import PageLayout from "../components/layout/PageLayout";
+import { DEVELOPERS } from "../constants/team";
+import Image from "next/image";
+import styles from "../styles/pages/developers.module.scss";
 
 function dev() {
-  return (
-    <PageLayout title='Developer Team | ACM at PEC' description='People behind this wonderful work of art website.'>
-        HARSH JOHAR, MANJOT, ABHINAV, ISHWAR, SANIL
-    </PageLayout>
-  )
+    return (
+        <PageLayout
+            title="Developer Team | ACM at PEC"
+            description="People behind this wonderful work of art website."
+            heading={"Our Dev team"}
+        >
+            <div className={styles.heads}>
+                <h3>Awesome hands behind this work of art</h3>
+                <div className={styles.cards}>
+                    {DEVELOPERS.map((head, i) => {
+                        return (
+                            <div className={`${styles.card}`} key={i}>
+                                <Image
+                                    height={200}
+                                    width={200}
+                                    src={head.image}
+                                    alt={head.name}
+                                />
+                                <div className={styles.details}>
+                                    <p className={styles.name}>{head.name}</p>
+                                    <p>{head.post}</p>
+                                    <div key={i} className={styles.tech}>
+                                        {head.tech.map((t, i) => {
+                                            return (
+                                                <Image
+                                                    width={30}
+                                                    height={30}
+                                                    src={t}
+                                                    alt={t}
+                                                    key={i}
+                                                />
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </PageLayout>
+    );
 }
 
-export default dev
+export default dev;
