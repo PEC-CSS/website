057bd8ed0c16bc87693a38033ef140a2b17f8e24
Ishwarendra Jha <ishwarendra27@gmail.com>
2023-09-02 12:54:31 +0530

    Embed image size fixed

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -61,14 +61,13 @@ function PageLayout({title, children, description, heading, bannerColor, footerC
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://www.pecacm.com/" />
                 <meta property="og:title" content={title} />
-                <meta property="og:description" content={description + "\n" + "We are a 300+ strong community of Computer Science enthusiasts working together to advance Computing as a Science and Profession. We are proud to be part of the official ACM Student Chapter - PEC ACM, and our base camp is at Punjab Engineering College, Chandigarh."} />
-                <meta property="og:image" content="https://www.pecacm.com/assets/logos/acm.png" />
+                <meta property="og:description" content={description} />
                 <meta property="og:image:type" content="image/png" />
-                <meta property="og:image:height" content="250" />
-                <meta property="og:image:width" content="250" />
+                <meta property="og:image:height" content="350" />
+                <meta property="og:image:width" content="350" />
                 <meta property='og:site_name' content='PEC CSS | ACM at PEC' />
 
-                <meta name="keywords" content="pecacm, acmcss, punjab engineering college, pec, pec cse, pec chandigarh, saasc, scc, drams, peb, eeb, heb, Technical Society of PEC, Coding Club of PEC, ai, cp, dev, socials, wit, cyber, punjabi editorial board pec, pecfest, iit chandigarh, iitc, chandigarh" />
+                <meta name="keywords" content="pecacm, acmcss, punjab engineering college, pec, pec cse, pec chandigarh, saasc, scc, drams, peb, eeb, heb, Technical Society of PEC, Coding Club of PEC, ai, cp, dev, socials, wit, cyber, punjabi editorial board pec, pecfest, iit chandigarh, iitc, chandigarh, pwoc, ideathon, codebattle, hackathons, codeblooded, kaggle, tips and tricks" />
 
                 {/* TODO : Verification pending */}
                 <meta name="google-site-verification" content="wJCASasAYLy_hM-ndSessna54e0JH7VCzmg5asx1aFc" />
