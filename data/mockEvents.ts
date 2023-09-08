2560ac8c80e05f2f1d11450345451763c6e71dee
Ishwarendra Jha <ishwarendra27@gmail.com>
2023-09-09 01:35:45 +0530

    Fix some event info

--- data/mockEvents.ts
+++ data/mockEvents.ts
@@ -58,10 +58,10 @@ export const mockEvents = <Event[]>[
         }).toDate(),
         title: "Lockout v2",
         resource: [
-            "cp", "DSA mock round for internships",
+            "cp", "1v1 Duel",
             "Platform: Discord Voice Channel",
-            "DSA and CP are essential for coding interviews and real-world software development challenges",
-            "Don't miss out on this opportunity. Mark your calendars and make sure to be there!",
+            "A 1v1 Coding Showdown where we feature the top two coders from the college, challenging them with the same set of problems to solve while they live-stream their screens, allowing everyone to witness their coding and bug-fixing skills in real-time.",
+            "Mark your calendars and make sure to be there!",
             "",
         ],
     },
@@ -108,7 +108,7 @@ export const mockEvents = <Event[]>[
         resource: [
             "cp", "DSA mock round for internships",
             "Platform: Hackerearth",
-            "DSA and CP are essential for coding interviews and real-world software development challenges",
+            "We are happy to bring you the sequel of CodeBattle_2023.1 having the exactly same rules as last time.",
             "Don't miss out on this opportunity. Mark your calendars and make sure to be there!",
             "",
         ],
@@ -196,7 +196,7 @@ export const mockEvents = <Event[]>[
             hour: 19,
             minute: 0,
         }).toDate(),
-        title: "Crewspehere Keynote Session",
+        title: "Crewsphere Keynote Session",
         resource: [
             "dev", "Introduction to Web3",
             "Platform: L21",
@@ -222,10 +222,10 @@ export const mockEvents = <Event[]>[
         }).toDate(),
         title: "Intro to Competitive Programming",
         resource: [
-            "cp", "DSA mock round for internships",
+            "cp", "Getting Started with CP",
             "Platform: L21",
-            "DSA and CP are essential for coding interviews and real-world software development challenges",
-            "Don't miss out on this opportunity. Mark your calendars and make sure to be there!",
+            "Unlock your coding potential with our Competitive Programming introductory session. Develop problem-solving skills, code efficiently, and open doors to tech career opportunities. ",
+            "Join us on this exciting journey to become a skilled competitive programmer.",
             "",
         ],
     },
@@ -246,7 +246,7 @@ export const mockEvents = <Event[]>[
         }).toDate(),
         title: "MLtiverse - Hands on ML",
         resource: [
-            "ai", "Kaggle Challenge Discussion",
+            "ai", "Build your very first ML model",
             "Platform: L21",
             "AI/ML are highly in demand skills due to their critical role in solving complex problems across industries",
             "Don't miss out on this opportunity. Mark your calendars and make sure to be there!",
@@ -270,7 +270,7 @@ export const mockEvents = <Event[]>[
         }).toDate(),
         title: "Intro to Git/github",
         resource: [
-            "dev", "Introduction to version control",
+            "dev", "Introduction to Version Control System",
             "Platform: L21",
             "Learning git and github is important for collaboration, and open-source development, enhancing employability and project management skills",
             "Don't miss out on this opportunity. Mark your calendars and make sure to be there!",
@@ -296,8 +296,8 @@ export const mockEvents = <Event[]>[
         resource: [
             "dev", "Introduction to Web Development",
             "Platform: L21",
-            "Web is the future, and we want you to be at the forefront of this revolutionary technology.",
-            "Don't miss out on this opportunity to enhance your skills and secure your academic success. Mark your calendars and make sure to be there! We have exciting surprises for everyone.",
+            "Discover the world of web development! Our workshop will introduce you to the foundations of building websites.",
+            "Get ready to create and code for the web!",
             "",
         ],
     }
