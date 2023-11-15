d23da12494fb7eebdc73b6072494b38d3988f7d1
Aaditya <aadityavardhanvij.bt22cse@pec.edu.in>
2023-11-15 11:30:13 +0530

    endEventUIFixed

--- components/common/EventPopup/PillContainer.tsx
+++ components/common/EventPopup/PillContainer.tsx
@@ -10,12 +10,13 @@ export default function PillContainer({ pills, handleDelete }: Props) {
     return <div style={{
         display:"flex",
         flexWrap:"wrap",
-        border:"dotted",
-        borderColor:"lightpink",
-        borderRadius:"10px",
-        padding:"4px",
-        minHeight:"40px",
-        minWidth:"150px"
+        // border:"dotted",
+        border: "none",
+        // borderColor:"lightpink",
+        // borderRadius:"10px",
+        // padding:"4px",
+        // minHeight:"40px",
+        // width: "100%",
     }}>
         {pills.map( (pill) => <PillComponent key={Math.random()} pill={pill} handleDelete={handleDelete} />)}
     </div>
