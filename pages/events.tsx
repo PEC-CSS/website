a14b2a56fcf9f3a0fcae6299f41c454a5bb84751
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-11-21 23:46:46 +0530

    Add useEffect hook to fetch events and update calendar

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,101 +1,146 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PageLayout from "../components/layout/PageLayout";
-import {
-  Calendar,
-  Event,
-  momentLocalizer,
-} from "react-big-calendar";
+import { Calendar, Event, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 
 const localizer = momentLocalizer(moment);
 
 import styles from "../styles/pages/events.module.scss";
 import "react-big-calendar/lib/css/react-big-calendar.css"; // calendar css
-import { mockEvents } from "../data/mockEvents";
 import EventPopup from "../components/common/EventPopup/EventPopup";
+import { EventResource } from "../types/eventResource";
+import { getEvents } from "../repository/events";
+import { Event as EventObject } from "../types/event";
 
 // see eventPropGetter
 const getEventClassByEvent = (event: Event) => {
-  let modifierStr = "";
-  if (event.resource[0]) {
-    const commitee = event.resource[0];
-    modifierStr = `rbc-override-${commitee}`;
-  }
-  return {
-    className: `rbc-override-event ${modifierStr}`,
-  };
+    let modifierStr = "";
+    if (event.resource.branch) {
+        const commitee = event.resource.branch;
+        modifierStr = `rbc-override-${commitee.toLowerCase()}`;
+    }
+    return {
+        className: `rbc-override-event ${modifierStr}`,
+    };
 };
 
 function Events() {
-  const [listedEvents, setListedEvents] = useState<Event[] | undefined>(
-    mockEvents
-  );
-  const [activeEvent, setActiveEvent] = useState<Event | null>(null);
-  const [eventResource, setEventResource] = useState<string[]>([
-    "acm",
-    "",
-    "",
-    "",
-  ]);
-  const [showModal, setShowModal] = useState<Boolean>(false);
+    const [listedEvents, setListedEvents] = useState<Event[] | undefined>([]);
+    const [activeEvent, setActiveEvent] = useState<Event | null>(null);
+    const [eventResource, setEventResource] = useState<EventResource | null>(
+        null
+    );
+    const [showModal, setShowModal] = useState<Boolean>(false);
 
-  const handleSelectEvent = (event: Event) => {
-    setEventResource(event ? event.resource : ["acm", "", "", ""]);
-    setActiveEvent(event);
-    setShowModal(true);
-  };
+    const [currDate, setCurrDate] = useState(new Date(Date.now()));
 
+    const handleSelectEvent = (event: Event) => {
+        setEventResource(event.resource);
+        setActiveEvent(event);
+        setShowModal(true);
+    };
 
-  return (
-    <PageLayout
-      title="Events | ACM at PEC"
-      heading="Events"
-      description="Upcoming and Ongoing events at PEC ACM"
-    >
-      <div className={styles.events}>
-        <p>Have a look at our calendar</p>
-        <div className={styles.calendarWrapper}>
-          {/* {showModal ? (
-            <EventPopup
-              handleClose={() => {
-                setShowModal(false);
-              }}
-              title={activeEvent?.title as string}
-              subTitle={eventResource[1]}
-              description={eventResource.slice(2, eventResource.length - 1)}
-              imageUrl={
-                eventResource[eventResource.length - 1].length === 0
-                  ? `/assets/logos/${eventResource[0]}.png`
-                  : eventResource.slice(-1)[0]
-              }
-              startDate={activeEvent?.start ? activeEvent.start : new Date()}
-              endDate={activeEvent?.end ? activeEvent.end : new Date()}
-            />
-          ) : (
-            <></>
-          )} */}
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const res = await getEvents({
+                startDate: moment(currDate)
+                    .subtract(1, "month")
+                    .startOf("month")
+                    .format("YYYY-MM-DD"),
+                endDate: moment(currDate)
+                    .add(1, "month")
+                    .endOf("month")
+                    .format("YYYY-MM-DD"),
+            });
 
-          <Calendar
-            localizer={localizer}
-            events={listedEvents}
-            className={styles.calendar}
-            popup={true}
-            onSelectEvent={handleSelectEvent}
-            startAccessor={(event) => new Date(event.start ?? Date.now())}
-            endAccessor={(event) => new Date(event.end ?? Date.now())}
-            eventPropGetter={getEventClassByEvent}
-            views={{
-              month: true,
-              week: false,
-              day: false,
-              agenda: false,
-            }}
-            dayLayoutAlgorithm={"no-overlap"}
-          />
-        </div>
-      </div>
-    </PageLayout>
-  );
+            if (res.error) {
+                return;
+            }
+
+            const calendarEvents = res.events?.map((event: EventObject) => {
+                return {
+                    start: moment(event.startDate).toDate(),
+                    end: moment(event.endDate).toDate(),
+                    title: event.title,
+                    resource: {
+                        id: event.id,
+                        branch: event.branch,
+                        venue: event.venue,
+                        description: event.description,
+                        relatedLink: event.relatedLink,
+                        ended: event.ended,
+                    } as EventResource,
+                };
+            });
+
+            setListedEvents(calendarEvents);
+        };
+
+        fetchEvents();
+    }, [currDate]);
+
+    return (
+        <PageLayout
+            title="Events | ACM at PEC"
+            heading="Events"
+            description="Upcoming and Ongoing events at PEC ACM"
+        >
+            <div className={styles.events}>
+                <p>Have a look at our calendar</p>
+                <div className={styles.calendarWrapper}>
+                    {showModal ? (
+                        <EventPopup
+                            id={eventResource?.id ?? 0}
+                            ended={eventResource?.ended ?? false}
+                            handleClose={() => {
+                                setShowModal(false);
+                            }}
+                            title={activeEvent?.title as string}
+                            description={eventResource?.description as string}
+                            imageUrl={`/assets/logos/${
+                                eventResource?.branch.toLowerCase() ?? "acm"
+                            }.png`}
+                            venue={eventResource?.venue ?? ""}
+                            startDate={
+                                activeEvent?.start
+                                    ? activeEvent.start
+                                    : new Date()
+                            }
+                            endDate={
+                                activeEvent?.end ? activeEvent.end : new Date()
+                            }
+                            listedEvents={listedEvents}
+                            setListedEvents={setListedEvents}
+                        />
+                    ) : (
+                        <></>
+                    )}
+
+                    <Calendar
+                        date={currDate}
+                        onNavigate={(date) => {
+                            setCurrDate(date);
+                        }}
+                        localizer={localizer}
+                        events={listedEvents}
+                        className={styles.calendar}
+                        popup={true}
+                        onSelectEvent={handleSelectEvent}
+                        startAccessor="start"
+                        endAccessor="end"
+                        eventPropGetter={getEventClassByEvent}
+                        views={{
+                            month: true,
+                            week: false,
+                            day: false,
+                            agenda: false,
+                        }}
+                        dayLayoutAlgorithm={"no-overlap"}
+                    />
+                </div>
+            </div>
+        </PageLayout>
+    );
 }
 
 export default Events;
